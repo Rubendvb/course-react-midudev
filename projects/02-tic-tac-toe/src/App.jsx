8e78cd724698dba79cd712ee4eb71e171efc3283
Rubendvb <rubenvasquez556@gmail.com>
2023-07-30 21:14:39 -0300

    organizando funções

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,16 +6,27 @@ import WinnerModal from './components/WinnerModal'
 
 import { TURNS } from './assets/js/constants'
 import { checkWinnerFrom, checkEndGame } from './assets/js/board'
+import { saveGameToStorage, resetGameToStorage } from './assets/js/storage'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+
+    return turnFromStorage ?? TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameToStorage()
   }
 
   const updateBoard = (index) => {
@@ -34,6 +45,9 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
 
     setTurn(newTurn)
+
+    // guardar partida no localStorage
+    saveGameToStorage({ board: newBoard, turn: newTurn })
 
     // revisar se tem ganhador
     const newWinner = checkWinnerFrom(newBoard)
